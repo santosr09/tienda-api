7c95776ba8139174271a0408e758757f0c6c5726
santosr09 <santos.r09@gmail.com>
2024-04-27 10:29:41 -0600

    Adding project structure: Spring boot, REST, JPA, Postrgesql, flyway

---
+++ src/main/java/com/jsantos/tiendaapi/TiendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsantos.tiendaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsantos/tiendaapi/TiendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsantos.tiendaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
